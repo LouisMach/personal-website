578aea45c3d2444510a2d2d95fde3dbb1cee383a
Louis Machiels <lmachiels@ilionx.com>
2022-12-04 11:46:08 +0100

    Updated title, changed App from function to class.

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import TextAnimation from './components/TextAnimation';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          I write
-        </p>
-        <a
-          className="App-link"
-          href="https://www.linkedin.com/in/louis-machiels/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          LinkedIn
-        </a>
-        <a
-          className="App-link"
-          href="https://github.com/LouisMach/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          GitHub
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props: {}) {
+    super(props);
+    this.state = { textIndex: 0 };
+  }
+
+  // useEffect(() => {
+  //   const interval = setInterval(() => {
+  //     console.log('This will run every second!');
+  //   }, 1000);
+  //   return () => clearInterval(interval);
+  // }, []);
+
+  render() {
+    return (
+      <div className="App" >
+        <header className="App-header">
+          <p>
+            I write <TextAnimation></TextAnimation>
+          </p>
+          <a
+            className="App-link"
+            href="https://www.linkedin.com/in/louis-machiels/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            LinkedIn
+          </a>
+          <a
+            className="App-link"
+            href="https://github.com/LouisMach/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            GitHub
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
